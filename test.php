e5efbe1867c0aa0cccbf30731520e1240051f376
Edgars Urbans <edgars.urbans@gmail.com>
2017-12-18 00:24:43 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,29 +6,29 @@
 	<link rel="stylesheet" type="text/css" href="./style.css"/>
 	<script type="text/javascript" src="./test.js"></script>
 </head>
-<body>
+    <body>
 	<?php
 		$dbconn = pg_connect("host=localhost port=8887 dbname=Test user=postgres password=123") or die("Could not connect");
 
 		$questions = pg_query($dbconn, "SELECT questionid, question, answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10 from questions WHERE testname = '".$_POST['testName']."'");
-    $rows = pg_num_rows($questions);
+    		$rows = pg_num_rows($questions);
 
-    $result = pg_query($dbconn, "INSERT INTO users (username, testname) VALUES ('".$_POST['userName']."', '".$_POST['testName']."') RETURNING id");
-    $row = pg_fetch_row($result);
-    $id = $row[0];
+    		$result = pg_query($dbconn, "INSERT INTO users (username, testname) VALUES ('".$_POST['userName']."', '".$_POST['testName']."') RETURNING id");
+    		$row = pg_fetch_row($result);
+    		$id = $row[0];
   			
-  	echo "<script>getData(".$rows.",'".$_POST['userName']."',".$id.")</script>";
-
-  	for($i=0; $i<$rows; $i++){
-      $arr = pg_fetch_assoc($questions, $i);
-  		foreach($arr as $value){
-  			if($value != null){
-  				echo "<script>getQuestions('".$value."',".$i.")</script>";
+  		echo "<script>getData(".$rows.",'".$_POST['userName']."',".$id.")</script>";
+	    
+  		for($i=0; $i<$rows; $i++){
+      		$arr = pg_fetch_assoc($questions, $i);
+  			foreach($arr as $value){
+  				if($value != null){
+  					echo "<script>getQuestions('".$value."',".$i.")</script>";
+  				}
   			}
   		}
-  	}
-    pg_close($dbconn);
+    		pg_close($dbconn);
 	?>
 	<script>createForm();</script>
-</body>
+  </body>
 </html>
