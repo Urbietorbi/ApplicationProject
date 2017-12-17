e439ac1e6e19631dd052ea9f6400f54e64ca1aa3
Edgars Urbans <edgars.urbans@gmail.com>
2017-12-18 00:37:59 +0200

    Update sendData.php

--- sendData.php
+++ sendData.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
+	<head>
+		<meta charset="UTF-8"/>
+	</head>
+	<body>
 
-</head>
-<body>
-
-<?php
-	$dataType = $_GET['dataType'];
-	$con = pg_connect("host=localhost port=8887 dbname=Test user=postgres password=123") or die("Could not connect");
-	if($dataType == "answer"){
-   		pg_query($con, "UPDATE users SET ".$_GET['questionId']." = ".$_GET['globalId']." WHERE id = ".$_GET['userId']."");
-    	pg_close($con);
-    }
-    if($dataType == "score"){
-    	pg_query($con, "UPDATE users SET score = ".$_GET['score'].", total = ".$_GET['total']." WHERE id = ".$_GET['userId']."");
-    	pg_close($con);
-    }
-?>
-</body>
+		<?php
+			$dataType = $_GET['dataType'];
+			$con = pg_connect("host=localhost port=8887 dbname=Test user=postgres password=123") or die("Could not connect");
+			if($dataType == "answer"){
+   				pg_query($con, "UPDATE users SET ".$_GET['questionId']." = ".$_GET['globalId']." WHERE id = ".$_GET['userId']."");
+    				pg_close($con);
+    			}
+    			if($dataType == "score"){
+    				pg_query($con, "UPDATE users SET score = ".$_GET['score'].", total = ".$_GET['total']." WHERE id = ".$_GET['userId']."");
+    				pg_close($con);
+    			}
+		?>
+	</body>
 </html>
