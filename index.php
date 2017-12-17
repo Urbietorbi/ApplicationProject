9dbd0b5f01ee6b3ffb2f192aa909b596c07d61d7
Edgars Urbans <edgars.urbans@gmail.com>
2017-12-18 00:32:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
- <html>
- <head>
- <meta charset="UTF-8"/>
- <title>Testa uzdevums kandidātam</title>
- <link rel="stylesheet" type="text/css" href="./style.css"/>
-  <?php
-    $dbconn = pg_connect("host=localhost port=8887 dbname=Test user=postgres password=123") or die("Could not connect");
-    $tests = pg_query($dbconn, "SELECT DISTINCT testname FROM questions");
-    $arr = pg_fetch_all_columns($tests);
-    pg_close($dbconn);
-  ?>
- </head>
- <body>
- <fieldset>
- <p style="font-size:40px"><b>Testa uzdevums</b></p>
- <form target="_self"; action="./test.php"; method="post">
-	   <input name="userName" required type="text" placeholder="Ievadi savu vārdu">
-	   <select name="testName" required>
-		  <option value="" disabled selected>Izvēlies testu..</option>
-		    <?php 
-          foreach($arr as $value) {
-            echo '<option value="'.$value.'">'.$value.'</option>';
-          }
-        ?>
-	   </select>
-	   <input type="submit" value="Sākt" style="width:15%; min-width: 100px">
-   </fieldset>
-</form>
-</body>
+ <!DOCTYPE html>
+ 	<head>
+		 <meta charset="UTF-8"/>
+ 		 <title>Testa uzdevums kandidātam</title>
+ 		 <link rel="stylesheet" type="text/css" href="./style.css"/>
+ 		 <?php
+    			$dbconn = pg_connect("host=localhost port=8887 dbname=Test user=postgres password=123") or die("Could not connect");
+    			$tests = pg_query($dbconn, "SELECT DISTINCT testname FROM questions");
+    			$arr = pg_fetch_all_columns($tests);
+    			pg_close($dbconn);
+  		?>
+ 	</head>
+ 	<body>
+ 		<fieldset>
+ 			<p style="font-size:40px"><b>Testa uzdevums</b></p>
+ 			<form target="_self"; action="./test.php"; method="post">
+	   			<input name="userName" required type="text" placeholder="Ievadi savu vārdu">
+	   			<select name="testName" required>
+		 			<option value="" disabled selected>Izvēlies testu..</option>
+		    			<?php 
+          					foreach($arr as $value) {
+            						echo '<option value="'.$value.'">'.$value.'</option>';
+         					 }
+        				?>
+	   			</select>
+	   			<input type="submit" value="Sākt" style="width:15%; min-width: 100px">
+			</form>
+		</fieldset>
+	</body>
 </html>
